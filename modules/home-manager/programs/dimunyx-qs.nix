b8838daa8d0b4bdbead54165eda0088af7115c60
dimunyx <botnarudumitru10@gmail.com>
2026-07-26 17:24:37 +0300

    Fixed locale issues v2

--- modules/home-manager/programs/dimunyx-qs.nix
+++ modules/home-manager/programs/dimunyx-qs.nix
@@ -6,9 +6,11 @@
 		dimunyx-qs = {
 			enable = true;
 			password = "1111";
+			locale = "ru_RU";
 			enableWallpapers = true;
 			weather = {
 				enable = true;
+				city = "Chisinau";
 				key = "328b152484b2e4e9ca5d80f07361dd30";
 			};
 		};
