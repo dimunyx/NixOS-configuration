4140076b3239830e964cd6d757856878869aa382
dimunyx <botnarudumitru10@gmail.com>
2026-08-05 00:07:11 +0300

    Changed color theme

--- homes/dimunyx/programs/spicetify.nix
+++ homes/dimunyx/programs/spicetify.nix
@@ -1,46 +1,46 @@
 { pkgs, lib, inputs, ... }: {
-	imports = [
-		inputs.spicetify-nix.homeManagerModules.default
-	];
-	programs = {
-		spicetify = 
-			let
-				spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
-			in {
-				enable = true;
-				theme = spicePkgs.themes.dribbblish;
-				enabledExtensions = with spicePkgs.extensions; [
-      					adblockify
-    				];
-				colorScheme = "custom";
-				customColorScheme = {
-					text = "CDD6F4";
-					subtext = "A6ADC8";
-					main = "1E1E2E";
-					main-elevated = "1E1E2E";
-					main-transition = "212232";
-					highlight = "272839";
-					highlight-elevated = "42435C";
-					sidebar = "1E1E2E";
-					player = "1E1E2E";
-					card = "1E1E2E";
-					shadow = "1E1E2E";
-					selected-row = "CDD6F4";
-					button = "89B4FA";
-					button-active = "89B4FA";
-					button-disabled = "89B4FA";
-					tab-active = "1E1E2E";
-					notification = "CBA6F7";
-					notification-error = "F38BA8";
-					misc = "1E1E2E";
-					play-button = "B4BEFE";
-					play-button-active = "B4BEFE";
-					progress-fg = "89B4FA";
-					progress-bg = "1E1E2E";
-					heart = "F38BA8";
-					pagelink-active = "E4D0FB";
-					radio-btn-active = "E4D0FB";
-			};
-		};
-	};
+        imports = [
+                inputs.spicetify-nix.homeManagerModules.default
+        ];
+        programs = {
+                spicetify =
+                        let
+                                spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+                        in {
+                                enable = true;
+                                theme = spicePkgs.themes.dribbblish;
+                                enabledExtensions = with spicePkgs.extensions; [
+                                        adblockify
+                                ];
+                                colorScheme = "custom";
+                                customColorScheme = {
+                                        text = "EBFAFA";
+                                        subtext = "ABB4DA";
+                                        main = "212337";
+                                        main-elevated = "212337";
+                                        main-transition = "222539";
+                                        highlight = "25283C";
+                                        highlight-elevated = "39405B";
+                                        sidebar = "212337";
+                                        player = "212337";
+                                        card = "212337";
+                                        shadow = "212337";
+                                        selected-row = "EBFAFA";
+                                        button = "37F499";
+                                        button-active = "37F499";
+                                        button-disabled = "37F499";
+                                        tab-active = "212337";
+                                        notification = "A48CF2";
+                                        notification-error = "F16C75";
+                                        misc = "212337";
+                                        play-button = "04D1F9";
+                                        play-button-active = "04D1F9";
+                                        progress-fg = "37F499";
+                                        progress-bg = "212337";
+                                        heart = "F16C75";
+                                        pagelink-active = "DBD1FA";
+                                        radio-btn-active = "DBD1FA";
+                                };
+                        };
+        };
 }
