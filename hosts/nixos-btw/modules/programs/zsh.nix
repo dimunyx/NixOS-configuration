9f75bec90535b92015ba248e729b62b3cd1fe784
dimunyx <botnarudumitru10@gmail.com>
2026-08-07 18:05:15 +0300

    Changed color to Catppuccin

--- hosts/nixos-btw/modules/programs/zsh.nix
+++ hosts/nixos-btw/modules/programs/zsh.nix
@@ -8,8 +8,8 @@
 			];
 			shellAliases = {
 				dev = "cd ~/dev";
-				matrix = "unimatrix --color green --speed 96";
-				cpufetch = "cpufetch --color 55,244,153:55,244,153:55,244,153:235,250,250:235,250,250";
+				matrix = "unimatrix --color blue --speed 96";
+				cpufetch = "cpufetch --color 137,180,250:137,180,250:137,180,250:205,214,244:205,214,244";
 				start-video = "gpu-screen-recorder -w screen -o '/home/dimunyx/Видео/Выходные видео/output.mp4' -c mp4 -f 60 -k h264 -q very_high -a default_output";
 			};
 			enableLsColors = true;
